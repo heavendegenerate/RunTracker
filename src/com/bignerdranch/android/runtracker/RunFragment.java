3954cefb82e32f0f45242f7ab0784d1b09d3ac5b
huoda <heavendegenerate@hotmail.com>
2015-12-14 21:54:14 +0800

    2015-12-14-2

--- src/com/bignerdranch/android/runtracker/RunFragment.java
+++ src/com/bignerdranch/android/runtracker/RunFragment.java
@@ -1,8 +1,6 @@
 package com.bignerdranch.android.runtracker;
 
 
-import com.bignerdbranch.android.runtracker.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
--- src/com/bignerdranch/android/runtracker/SingleFragmentActivity.java
+++ src/com/bignerdranch/android/runtracker/SingleFragmentActivity.java
@@ -1,7 +1,5 @@
 package com.bignerdranch.android.runtracker;
 
-import com.bignerdbranch.android.runtracker.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
